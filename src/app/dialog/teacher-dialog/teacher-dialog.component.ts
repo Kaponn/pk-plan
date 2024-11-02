2f758afb69c8ae275c9a1c2ea9fea63f46b460c6
Kacper Ledwoń <kacper@MacBook-Air-Kacper-2.local>
2024-11-02 21:41:32 +0100

    create models for teacher and classes

---
+++ src/app/dialog/teacher-dialog/teacher-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './teacher-dialog.component.html',
+  styleUrl: './teacher-dialog.component.scss'
+})
+export class TeacherDialogComponent {
+
+}
