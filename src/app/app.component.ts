34f8554fb86d20c6c3d23cf32499a256dbf89895
Kacper Ledwoń <43321190+Kaponn@users.noreply.github.com>
2024-11-24 03:51:34 +0100

    Sidebar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TeacherComponent } from './components/teacher/teacher.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TeacherComponent],
+  imports: [RouterOutlet, TeacherComponent, MatSidenavModule, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
