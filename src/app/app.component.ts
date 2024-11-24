5dc41e28b76453ae5d748d2a01ffc511f745ea4e
Kacper Ledwoń <kacper@MacBook-Air-Kacper-2.local>
2024-11-24 04:03:13 +0100

    style button to add teacher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TeacherComponent } from './components/teacher/teacher.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TeacherComponent, MatSidenavModule, SidebarComponent],
+  imports: [RouterOutlet, MatSidenavModule, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
